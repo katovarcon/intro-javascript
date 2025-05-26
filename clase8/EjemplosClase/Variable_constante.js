0eb012ee922b4a81363c6034c67374e046eefa93
katovarcon <k.tovarcontreras@hotmail.com>
2025-05-25 21:22:31 -0600

    agregando ejercicios de clase8

---
+++ clase8/EjemplosClase/Variable_constante.js
@@ -0,0 +1,15 @@
+// Declarar variables de tipo let y const
+let nombre = "Sofi";
+const edad = "29";
+
+//Concatenación de string (cadenas de texto)
+console.log("Hola, mi nombre es: " + nombre + " y tengo " + edad + " años. ")
+console.log(`Hola, mi nombre es: ${nombre} y tengo ${edad} años.`);
+
+//Cambiar el valor de una variable
+nombre = "Laura";
+console.log(`Ahora mi nombre es ${nombre} `);
+
+//Cambiar el valor de una constante
+edad = "20";
+console.log(`Ahora mi edad es ${edad} `);
